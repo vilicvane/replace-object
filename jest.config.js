78db756a24494c4965ee456f4162affaa16636a0
vilic <i@vilic.info>
2019-03-12 14:23:56 +0800

    Upgrade configs

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,4 @@
 module.exports = {
-  clearMocks: true,
-  coverageDirectory: 'coverage',
-  globals: {
-    'ts-jest': {
-      tsConfig: 'test/tsconfig.json',
-    },
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js'],
-  testMatch: ['<rootDir>/test/**/*.test.ts'],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
+  preset: 'ts-jest',
+  testEnvironment: 'node',
 };
